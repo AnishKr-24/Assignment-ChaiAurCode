a2e4d7e7350b87a0e5b4cadcd908071089d2c61e
Anish Kumar <anishkumar96084@gmail.com>
2026-03-18 22:06:13 +0530

    updated

--- src/03-assignment.js
+++ src/03-assignment.js
@@ -1,25 +1,27 @@
 export function fixBollywoodTitle(title) {
-
-    // validation
     if (typeof title !== "string") return "";
 
-    // trim and normalize spaces
     title = title.trim().replace(/\s+/g, " ");
-
     if (title === "") return "";
 
-    const smallWords = ["ka", "ki", "ke", "the", "a", "an", "of", "in"];
+
+    const smallWords = ["ka", "ki", "ke", "of", "the", "a", "an", "in"];
 
     const words = title.toLowerCase().split(" ");
 
-    const fixed = words.map((word, index) => {
+    const result = words.map((word, index) => {
+   
+        if (index === 0) {
+            return word.charAt(0).toUpperCase() + word.slice(1);
+        }
 
-        if (index !== 0 && smallWords.includes(word)) {
+        if (smallWords.includes(word)) {
             return word;
         }
 
+ 
         return word.charAt(0).toUpperCase() + word.slice(1);
     });
 
-    return fixed.join(" ");
+    return result.join(" ");
 }
